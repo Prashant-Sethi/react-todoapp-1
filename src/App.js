8683c7ce94ba5b2514945a8a51944a395c5f7cb9
Prashant Sethi <theseth.prashant@gmail.com>
2020-06-13 19:55:35 +0530

    To Do App Phase 2: List items writen as a component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from "react";
 
+import TodoItem from "./components/TodoItem";
+
 import "./App.css";
 
 function App() {
   return (
     <ul>
-      <li>
-        <input type="checkbox" name="" id="" />
-        <span>To Do 1</span>
-      </li>
-      <li>
-        <input type="checkbox" name="" id="" />
-        <span>To Do 2</span>
-      </li>
-      <li>
-        <input type="checkbox" name="" id="" />
-        <span>To Do 3</span>
-      </li>
-      <li>
-        <input type="checkbox" name="" id="" />
-        <span>To Do 4</span>
-      </li>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </ul>
   );
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <li>
+      <input type="checkbox" name="" id="" />
+      <span>To Do 1</span>
+    </li>
+  );
+}
+
+export default TodoItem;
