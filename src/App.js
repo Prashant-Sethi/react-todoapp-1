35f4e55b69cd36e003bfafaae343041a36350f6f
Prashant Sethi <theseth.prashant@gmail.com>
2020-06-14 00:05:26 +0530

    To Do App Phase 4: Change App to a class based component with a state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
 import todosData from "./data/todosData";
 import TodoItem from "./components/TodoItem";
 
 import "./App.css";
 
-function getTodoList() {
-  const todoList = todosData.map((todo) => (
-    <TodoItem key={todo.id} todo={todo} />
-  ));
-  return todoList;
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todosData: todosData,
+    };
+  }
 
-function App() {
-  return <div className="todo-list">{getTodoList()}</div>;
+  getTodoList() {
+    const todoList = this.state.todosData.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} />
+    ));
+    return todoList;
+  }
+
+  render() {
+    return <div className="todo-list">{this.getTodoList()}</div>;
+  }
 }
 
 export default App;
