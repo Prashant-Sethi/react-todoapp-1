27fdfc0d77e307e6bdbc8d69303977cca4a046ec
Prashant Sethi <theseth.prashant@gmail.com>
2020-06-13 19:42:00 +0530

    To Do App Phase 1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <ul>
+      <li>
+        <input type="checkbox" name="" id="" />
+        <span>To Do 1</span>
+      </li>
+      <li>
+        <input type="checkbox" name="" id="" />
+        <span>To Do 2</span>
+      </li>
+      <li>
+        <input type="checkbox" name="" id="" />
+        <span>To Do 3</span>
+      </li>
+      <li>
+        <input type="checkbox" name="" id="" />
+        <span>To Do 4</span>
+      </li>
+    </ul>
+  );
+}
+
+export default App;
